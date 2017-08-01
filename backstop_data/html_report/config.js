984d6eac09e4bcaeca95fb9c3a93cac4f6412ec0
Jesse Hattabaugh <jesse@studyblue.com>
2017-08-01 16:04:16 -0700

    init

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,68 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Logged_Out_HomePage_0_body_0_phone.png",
+        "test": "../bitmaps_test/20170801-160258/prod_test_Logged_Out_HomePage_0_body_0_phone.png",
+        "selector": "body",
+        "fileName": "prod_test_Logged_Out_HomePage_0_body_0_phone.png",
+        "label": "Logged Out HomePage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Logged_Out_HomePage_0_body_1_tablet_v.png",
+        "test": "../bitmaps_test/20170801-160258/prod_test_Logged_Out_HomePage_0_body_1_tablet_v.png",
+        "selector": "body",
+        "fileName": "prod_test_Logged_Out_HomePage_0_body_1_tablet_v.png",
+        "label": "Logged Out HomePage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Logged_Out_HomePage_0_body_2_tablet_h.png",
+        "test": "../bitmaps_test/20170801-160258/prod_test_Logged_Out_HomePage_0_body_2_tablet_h.png",
+        "selector": "body",
+        "fileName": "prod_test_Logged_Out_HomePage_0_body_2_tablet_h.png",
+        "label": "Logged Out HomePage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
